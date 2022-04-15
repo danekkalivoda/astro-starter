3f213c93f4b94692e42adb84cdc27e5983994291
Daniel Kalivoda <danek.kalivoda@hey.com>
2022-04-15 20:06:06 +0200

    astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,38 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import astroImagePlugin from "astro-imagetools/plugin";
 function CustomHmr() {
   return {
-    name: 'custom-hmr',
-    enforce: 'post',
+    name: "custom-hmr",
+    enforce: "post",
     // HMR
     handleHotUpdate({ file, server }) {
-      if (file.endsWith('.astro')) {
-        console.log('full reload...');
+      if (file.endsWith(".astro")) {
+        console.log("full reload...");
 
         server.ws.send({
-          type: 'full-reload',
-          path: '*'
+          type: "full-reload",
+          path: "*",
         });
       }
     },
-  }
+  };
 }
-
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind({ path: './tailwind.config.js' })],
+  integrations: [
+    tailwind({
+      config: {
+        path: "./tailwind.config.js",
+        applyAstroPreset: false
+      },
+    }),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
     },
-    plugins: [
-      CustomHmr()
-    ]
+    plugins: [CustomHmr(), astroImagePlugin],
   },
 });
